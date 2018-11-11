b6a0ce56cfd2ed3d549b0c196ca143706aa7e0be
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-11-11 21:11:07 +0100

    edited to handle arrays, strings or a mix of both as input

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 // func below returns a function that takes a list of classes as an argument
 // and turns it in an array with the spread operator and reduces it into a spaced string
 
-const classLister = styleObject => (...classList) => classList.reduce((list, myClass) => {
-  let output = list;
-  if (styleObject[myClass]) {
-    if (list) output += ' '; // appends a space if list is not empty
-    output += styleObject[myClass];
-  }
-  // Above: append 'myClass' to the list if it exists in styleObject
-  return output;
-}, '');
+const classLister = styleObject => (...classList) => {
+  const generateClassString = (list, myClass) => {
+    let output = list;
+    if (output) {
+      output += ' '; // appends a space if list is not empty
+    }
+    if (Array.isArray(myClass)) {
+      output += myClass.reduce(generateClassString, ''); // recursion to deal with Arrays
+    } else if (styleObject[myClass]) {
+      output += styleObject[myClass];
+      // append styleObject['myClass'] value to the list if it is defined in styleObject
+    } else if (typeof myClass === 'string') {
+      output += myClass; // append 'myClass' directly to the list
+    }
+
+    return output;
+  };
+  classList.reduce(generateClassString, '');
+};
 
 export default classLister;
--- test/index.js
+++ test/index.js
@@ -1,30 +1,74 @@
 import { assert } from 'chai';
 import classLister from '../src';
 
-const sampleStyleObject = {
-  App: 'App_App__3TjUG',
-  'd-flex-c': 'App_d-flex-c__xpDp1',
-};
-
 describe('Class lister test.', () => {
   it('should return a function scoped to a style object', () => {
     const sampleStyleObject = {
       App: 'App_App__3TjUG',
       'd-flex-c': 'App_d-flex-c__xpDp1',
+      redDiv: 'App_redDiv_fg3AS',
+      blueDiv: 'App_blueDiv_jYaS7',
     };
+
     assert(typeof classLister(sampleStyleObject) === 'function', 'Check return statement');
   });
 
-  it('the returned function should return a spaced string of valid classes', () => {
+  it('the sub-function should return a string', () => {
     const sampleStyleObject = {
       App: 'App_App__3TjUG',
       'd-flex-c': 'App_d-flex-c__xpDp1',
+      redDiv: 'App_redDiv_fg3AS',
+      'blue-div': 'App_blue-div_jYaS7',
     };
     const classes = classLister(sampleStyleObject);
-    const expectedVal = 'App_App__3TjUG App_d-flex-c__xpDp1';
     assert(
-      classes('App', 'bold', 'app', 'div', 'd-flex-c') === expectedVal,
+      typeof classes('App', 'misspelt-class', 'redDiv', 'div-class', 'd-flex-c') === 'string',
+      'sub-function not returning string'
+    );
+  });
+  it('the sub-function should return a spaced string of valid classes', () => {
+    const sampleStyleObject = {
+      App: 'App_App__3TjUG',
+      'd-flex-c': 'App_d-flex-c__xpDp1',
+      redDiv: 'App_redDiv_fg3AS',
+      'blue-div': 'App_blue-div_jYaS7',
+    };
+    const classes = classLister(sampleStyleObject);
+    const expectedVal = 'App_App__3TjUG misspelt-class App_redDiv_fg3AS div-class App_d-flex-c__xpDp1';
+    assert(
+      classes('App', 'misspelt-class', 'redDiv', 'div-class', 'd-flex-c') === expectedVal,
       'sub-function not returning spaced string of valid classes'
+    );
+  });
+
+  it('sub-function should work with an array as input', () => {
+    const sampleStyleObject = {
+      App: 'App_App__3TjUG',
+      'd-flex-c': 'App_d-flex-c__xpDp1',
+      redDiv: 'App_redDiv_fg3AS',
+      blueDiv: 'App_blueDiv_jYaS7',
+    };
+    const classes = classLister(sampleStyleObject);
+    const expectedVal = 'App_App__3TjUG misspelt-class App_redDiv_fg3AS App_blue-div_jYaS7 App_d-flex-c__xpDp1';
+    assert(
+      classes(['App', 'misspelt-class', 'redDiv', 'blue-div', 'd-flex-c']) === expectedVal,
+      'sub-function not returning spaced string of valid classes'
+    );
+  });
+
+  it('sub-function should work with a mix of array and strings as input', () => {
+    const sampleStyleObject = {
+      App: 'App_App__3TjUG',
+      'd-flex-c': 'App_d-flex-c__xpDp1',
+      redDiv: 'App_redDiv_fg3AS',
+      blueDiv: 'App_blueDiv_jYaS7',
+    };
+    const classes = classLister(sampleStyleObject);
+    const expectedVal = 'App_App__3TjUG misspelt-class App_redDiv_fg3AS fake-class App_blue-div_jYaS7 App_d-flex-c__xpDp1';
+    assert(
+      classes(['App', 'misspelt-class'], 'redDiv', 'fake-class', ['blueDiv', 'd-flex-c'])
+        === expectedVal,
+      'sub-function not returning spaced string of valid classes when input is arrays and strings mixed'
     );
   });
 });
